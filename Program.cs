eba1a483bce561e4ca5e28322bf5e9f5ae99cf22
no <53334539+120percentistaken@users.noreply.github.com>
2025-07-22 11:54:14 +0800

    Introduction for a C# refresh course.Initial commit of ConsoleApp project with .NET 6.0 framework. Added project assets, NuGet cache, and compiled binaries.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//C# is a type check language, so we need to specify the type of variables
+//Console.WriteLine("Bill Gates");
+//string name = "Bill Gates";
+using System.Reflection.Metadata;
+
+var name = "Bill Gates"; // 'var' infers the type from the right-hand side
+Console.WriteLine(name);
+
+//data types 
+string richMan = "Jeff Bezos";//string is a series of chars or a collection of characters
+long networth = 196000000000; // int is a value type
+char charAlphabet = 'A'; // char is a value type, single character. Char
+//A string is made up of many characters. A character is a primitive, likewise, 
+//it doesn't "contain" any other items. A string is basically an array of characters.
+
+
+//strings 
+//string is a reference type, not a value type
